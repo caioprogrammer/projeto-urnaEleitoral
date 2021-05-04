092fcea3e7a42ddc9949389ec05d72ec8416fbdc
Caio Nogueira <81397501+caioprogrammer@users.noreply.github.com>
2021-05-04 15:39:39 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,77 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '77222.jpg',legenda: 'Vereador',small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: '99.jpg', legenda: 'Prefeito', small: false},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: '84.jpg', legenda: 'Prefeito', small: false},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    },
+
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '17',
+                nome: 'Bolsomorte',
+                partido: 'CVD',
+                vice: 'CIC',
+                fotos: [
+                    {url: 'bolsoretardado.jpg', legenda: 'Prefeito', small: false},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '24',
+                nome: 'Danilo Gentili',
+                partido: 'GKAY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'danilo.jpg', legenda: 'Prefeito', small: false},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
